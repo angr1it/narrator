20253dd8c51d97ac493a45fa291fa13045a6e5a5
gritsenko.andrey.w <gritsenko.andrey.w@gmail.com>
2025-06-15 05:47:22 +0300

    upd: token auth

--- app/core/auth/__init__.py
+++ app/core/auth/__init__.py
@@ -1,19 +1,17 @@
-from fastapi import Header, HTTPException, status
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from config import app_settings
 
+security = HTTPBearer()
 
-def get_token_header(authorization: str | None = Header(default=None)) -> str:
-    """Проверяет Bearer‑токен из заголовка Authorization."""
-    if authorization is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or missing token",
-        )
-    token = authorization.replace("Bearer ", "")
+
+def get_token_header(
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+) -> str:
+    token = credentials.credentials
     if token != app_settings.AUTH_TOKEN:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or missing token",
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or missing token"
         )
     return token
